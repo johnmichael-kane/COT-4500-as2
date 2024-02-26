28132755fad0feecc7314e9e81cddb6199104d9b
JohnMichael Kane <123655765+johnmichael-kane@users.noreply.github.com>
2024-02-25 20:41:37 -0500

    edited the specifications needed

--- src/main/assignment_2.py
+++ src/main/assignment_2.py
@@ -1,1 +1,31 @@
+"""
+1. using neville's method, fin the 2nd degree interpolating value for f(3.7) for the following set of data
+a. 
 
+ x     f(x)
+3.6    1.675
+3.8    1.436
+3.9    1.318
+
+2. Using newton's fowrard method, print out the polynomial approximations for degrees 1,2 and 3 using the following set of data
+a. (hint, create a talbe first) b.
+
+3. Using the results from 2, approximate f(7.3)?
+4. Using the divided differece method, print out the Hermite polynomial approxiamtion matrix
+x       f(x)        f'(x)
+3.6     1.675       -1.195
+3.8     1.436       -1.188
+3.9     1.318       -1.182
+
+5. Using cubic spline interpolation, solve for the following tusing the set of data:
+
+x       f(x)
+2       3  
+5       5
+8       7
+10      9
+
+a. Find matrix A
+b. Vector b
+c. Vector x
+"""
